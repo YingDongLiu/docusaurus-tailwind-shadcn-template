083f2b28f64daf6f33dfb8bc2dab452dc0f3df6a
劉 <open.yingdong@gmail.com>
2025-08-14 23:39:38 +0900

    no message

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
             ]
           }
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Docusaurus Tailwind Shadcn. Templates by <a href="https://github.com/namnguyenthanhwork" style="font-weight: bold;" target="_blank">Thành Nam Nguyễn</a>`
+        copyright: `Copyright © ${new Date().getFullYear()} Docusaurus Tailwind Shadcn. Templates by <a href="https://github.com/namnguyenthanhwork" style="font-weight: bold;" target="_blank">YingDong Liu</a>`
       },
       colorMode: {
         defaultMode: 'dark',
